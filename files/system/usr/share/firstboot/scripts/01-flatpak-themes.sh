456b34c89d4eeed16633295f24b08fa4804c248a
Matthew Samson <snakeeyes021@comcast.net>
2026-01-02 04:26:35 -0500

    feat: implement versioned first-boot dispatcher for Flatpak overrides

---
+++ files/system/usr/share/firstboot/scripts/01-flatpak-themes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Apply Flatpak overrides system-wide
+flatpak override --filesystem=xdg-data/themes
+flatpak mask org.gtk.Gtk3theme.adw-gtk3
+flatpak mask org.gtk.Gtk3theme.adw-gtk3-dark
