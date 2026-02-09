58c2641343a99a2e217ccd77a880394664448a45
Matthew Samson <snakeeyes021@comcast.net>
2026-02-08 23:03:56 -0500

    fixed pixi path export

--- files/system/etc/profile.d/pixi-path.sh
+++ files/system/etc/profile.d/pixi-path.sh
@@ -1,5 +1,3 @@
 # pixi-path.sh
-# Add pixi global bin to PATH if it exists
-if [ -d "$HOME/.pixi/bin" ] ; then
-    PATH="$HOME/.pixi/bin:$PATH"
-fi
+# Always add pixi bin to PATH so it works immediately after first install
+export PATH="$HOME/.pixi/bin:$PATH"
